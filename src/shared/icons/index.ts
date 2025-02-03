f780415d4eae87b6be793137ada2a695a964545b
PassQwerty <vdwork0@gmail.com>
2025-02-03 15:19:13 +0300

    Изменил историю иконок

---
+++ src/shared/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CloseIcon } from "./CloseIcon.vue";
+export { default as DuplicateIcon } from "./DuplicateIcon.vue";
+export { default as ArrowUp } from "./ArrowUp.vue";
+export { default as ArrowDown } from "./ArrowDown.vue";
