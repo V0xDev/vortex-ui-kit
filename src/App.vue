2e2815d418bdbd08016413de8ad234b9d6ef4cc4
PassQwerty <vdwork0@gmail.com>
2025-02-15 18:40:46 +0300

    почистил app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
-<script setup lang="ts">
-import { shallowRef } from "vue";
-import VTextarea from "./components/Textarea/VTextarea.vue";
-import VInput from "./components/Input/VInput.vue";
-
-const value = shallowRef("");
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="info-container">
-    <div class="info-container__wrapper">
-      <!-- <VTextarea
-        v-model="value"
-        placeholder="фыв"
-        color="primary"
-        readonly
-        is-stretch
-        is-rounded
-      >
-        <template #before> before </template>
-        <template #after> after </template>
-      </VTextarea> -->
-    </div>
+    <div class="info-container__wrapper"></div>
   </div>
 </template>
 
