afb9f2735352de827f444ebfd441da993b05300c
PassQwerty <vdwork0@gmail.com>
2025-02-21 16:04:56 +0300

    почистил app.vue, подредактировал readme

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,13 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import VTable from "./components/Table/VTable.vue";
-import VTBody from "./components/Table/VTBody.vue";
-import VTCell from "./components/Table/VTCell.vue";
-import VTHead from "./components/Table/VTHead.vue";
-import VTRow from "./components/Table/VTRow.vue";
-import { data } from "./stories/Table/store";
-
-const select = ref();
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="info-container">
-    <div class="info-container__wrapper">
-      <VTable>
-        <VTHead is-sticky>
-          <VTRow
-            class="set-style-row"
-            is-row-header
-            background-color="primary"
-            :is-hover="false"
-          >
-            <VTCell border-color="primary">Header 1</VTCell>
-            <VTCell border-color="primary">Header 2</VTCell>
-            <VTCell border-color="primary">Header 3</VTCell>
-          </VTRow>
-        </VTHead>
-        <VTBody is-striped background-color="primary">
-          <VTRow
-            class="set-style-row"
-            background-color="primary"
-            v-for="item in data"
-            :is-select="item.id === select"
-            @click="select = item.id"
-            :key="item.id"
-            is-hover
-          >
-            <VTCell border-color="primary">{{ item.id }}</VTCell>
-            <VTCell border-color="primary">{{ item.title }}</VTCell>
-            <VTCell border-color="primary">{{ item.description }}</VTCell>
-          </VTRow>
-        </VTBody>
-      </VTable>
-    </div>
+    <div class="info-container__wrapper"></div>
   </div>
 </template>
 
 <style lang="scss">
 @use "@/assets/_index.scss" as *;
-
-.set-style-row {
-  display: grid;
-  grid-template-columns: 150px 150px 1fr;
-}
-
-.info-container__wrapper {
-  width: 1000px;
-  height: 700px;
-}
-
 #app,
 body,
 html,
@@ -75,5 +24,10 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.info-container__wrapper {
+  width: 1000px;
+  height: 700px;
 }
 </style>
