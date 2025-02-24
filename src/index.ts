71174015103757e510710d394e1b24daf498a105
PassQwerty <vdwork0@gmail.com>
2025-02-24 22:30:43 +0300

    промежуточные изменения перед публикацией v2.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { App, Plugin } from "vue";
+import * as components from "./components/index";
+
 export * from "./components/index";
 export * from "@/utils/Select";
 export * from "@/hooks/useRefDebounced";
 export * from "@/shared/constants";
 export * from "@/shared/types";
+
+export const VortexUI: Plugin = {
+  install(app: App) {
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component);
+    });
+  },
+};
